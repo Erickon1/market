be7c5ad295abac55de458f7045113bf54d73f907
ecologia <erick.roberto.mercado.arroyo@gmail.com>
2020-06-09 20:04:42 -0500

    add aop and security

--- src/main/java/mx/escapeSchool/market/domain/Product.java
+++ src/main/java/mx/escapeSchool/market/domain/Product.java
@@ -10,7 +10,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.ToString;
 
+@ToString(includeFieldNames = true)
 @Entity
 public class Product extends AuditModel{
 	
